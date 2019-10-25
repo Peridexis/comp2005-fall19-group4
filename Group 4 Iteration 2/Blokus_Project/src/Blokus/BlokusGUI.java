5a2a04d6e2d174221f00a550df7cce48502b1415
Andrew B <peridexis@protonmail.com>
2019-10-25 04:47:04 -0230

    updated code files and added completed jar

--- Group 4 Iteration 2/Blokus_Project/src/Blokus/BlokusGUI.java
+++ Group 4 Iteration 2/Blokus_Project/src/Blokus/BlokusGUI.java
@@ -10,50 +10,66 @@ import java.io.File;
 public class BlokusGUI {
 	public static final int Num_Player = 4;
 	
-	public static class Blokus_Frame extends JFrame{
+	public static class Blokus_Frame extends JFrame {
 		
 		private BlokusBoard board;
-	    private BlokusPlayer[] players;
+		private BlokusPlayer[] players;
 		private JButton instruction;
+		private JButton colorblindButton;
+		public boolean colorblind;
 		
 		public Blokus_Frame() {
-			
 			JFrame window = new JFrame("Blokus");
 			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		    window.setSize(800,800);
-		    window.setLocation(100, 100);
-		    JPanel panel1 = new JPanel();
-		    JPanel panel2 = new JPanel();
-		    window.add(panel1, BorderLayout.NORTH);
-		    window.add(panel2, BorderLayout.WEST);
-		    JButton button = new JButton("Instructions");
-		    panel2.add(button, BorderLayout.NORTH);
-//			setVisible(true);
+			window.setSize(800,800);
+			window.setLocation(100, 100);
 			window.setBackground(Color.gray);
-			JLabel l = new JLabel("Welcome to Blokus");
-			panel1.add(l);
-			button.addActionListener(new ButtonClickListener());
-			
-			
 			window.setVisible(true);
-			
+			setResizable(false); //设置窗体是否可以改变大小 
 
-			
-			
-			setResizable(false);//设置窗体是否可以改变大小 
-			
-		
-			
+			JPanel titlePanel = new JPanel();
+			JLabel titleLabel = new JLabel("Welcome to Blokus");
+			titlePanel.add(titleLabel);
+			window.add(titlePanel, BorderLayout.NORTH);
+
+			instruction = new JButton("Instructions");
+			instruction.addActionListener(new InstructionButtonListener());
+			colorblindButton = new JButton("Colorblind: Off");
+			colorblindButton.addActionListener(new ColorblindButtonListener());
+			colorblind = false;
+
+			JPanel menuPanel = new JPanel();
+			menuPanel.add(instruction, BorderLayout.EAST);
+			menuPanel.add(colorblindButton, BorderLayout.EAST);
+			window.add(menuPanel, BorderLayout.WEST);
 		}
 		
-	class ButtonClickListener implements ActionListener{
-
+	class InstructionButtonListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			JOptionPane.showMessageDialog(null, "\"RULES: Each player starts with 21 pieces. Turn order is: Blue, Yellow, Red, Green.\"\n\" A player's piece bin will highlight to indicate current turn.\" \n\" In the first round, players place their opening piece on a corner of the board.\" \n Each new piece played must be placed so that it touches at least one piece of the same color.\"\n\" only corner-to-corner contact allowed, edges cannot touch.\"\n\" A player may pass, if they have no valid moves, play continues.\"\n\" Game ends when no player can place piece.\"\n\" Score calculated by remaining pieces in players hands.\"\n\" The player with the highest score wins!\"");
+			JOptionPane.showMessageDialog(null, 
+				"\"RULES: Each player starts with 21 pieces. Turn order is: Blue, Yellow, Red, Green.\"\n" +
+				"\"A player's piece bin will highlight to indicate current turn.\" \n" +
+				"\"In the first round, players place their opening piece on a corner of the board.\" \n" +
+				"\"Each new piece played must be placed so that it touches at least one piece of the same color.\"\n" +
+				"\"Only corner-to-corner contact allowed, edges cannot touch.\"\n" +
+				"\"A player may pass, if they have no valid moves, play continues.\"\n" +
+				"\"Game ends when no player can place piece.\"\n" +
+				"\"Score calculated by remaining pieces in players hands.\"\n" +
+				"\"The player with the highest score wins!\""
+			);
 		}
-		 
+	}
+
+	class ColorblindButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent evnt) {
+			colorblind = !colorblind;
+			if (colorblind) {
+				colorblindButton.setText("Colorblind: On");
+			} else {
+				colorblindButton.setText("Colorblind: Off");
+			}
+		}
 	}
 		
 //		public Blokus_Frame()
@@ -121,13 +137,10 @@ public class BlokusGUI {
 			
 		
 		}
-		
 	
 	public static void main(String[] args)
-	   {
-	      new Blokus_Frame();
-	   }
-	
-	
+	{
+		new Blokus_Frame();
+	}
 }
 
